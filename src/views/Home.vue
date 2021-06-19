9be04cedf02d680380556d7484dd28660ef30297
LZY <2711908259@qq.com>
2021-06-19 22:19:10 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <h2>欢迎使用实验室管理系统</h2>
+    <!-- <div>
+    <src key="../assets/login-background.jpg"></src>
+    </div> -->
+  </div>
+</template>
+
+<script>
+// import * as api from "../api/news";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  // methods: {
+  //   query() {
+  //     api.get().then(res => {
+  //       this.tableData = res;
+  //     });
+  //   },
+  //   openNews(url) {
+  //     window.open(url, "_blank");
+  //   }
+  // },
+  created() {
+    this.query();
+  }
+};
+</script>
+
+<style scoped>
+.home-wrap {
+  height: 100%;
+  width: 100%;
+}
+
+.main-wrap {
+  margin-top: 10px;
+  height: 100%;
+  width: 100%;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+.home-container {
+  height: 100%;
+  width: 90%;
+  margin: auto;
+  background-image: url("../assets/login-background.jpg");
+}
+
+.home-aside {
+  height: 100%;
+}
+
+.aside-container {
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  height: 200px;
+  margin-top: 20px;
+}
+
+.aside-img {
+  width: 100%;
+}
+
+.aside-title {
+  color: #333;
+  font-size: 18px;
+}
+
+.aside-content {
+  font-size: 12px;
+  color: #999;
+}
+</style>
